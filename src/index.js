fc7369b16ddda3558e84edc966d5dfa2cdc70867
mithun-ctrl <itsmithun01@gmail.com>
2026-02-21 01:43:39 +0530

    Restore health endpoint server

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import { connectDB } from "./config/database.js";
 import { bot } from "./config/bot.js";
+import { createHealthServer } from "./config/healthServer.js";
+
+const healthServer = createHealthServer();
 
 async function bootstrap() {
+  await healthServer.start();
   await connectDB();
 
   bot.catch((error, ctx) => {
@@ -22,6 +26,7 @@ async function shutdown(signal) {
   try {
     bot.stop(signal);
     await mongoose.connection.close(false);
+    await healthServer.stop();
   } catch (error) {
     console.error("[APP] Shutdown error:", error.message);
   } finally {
